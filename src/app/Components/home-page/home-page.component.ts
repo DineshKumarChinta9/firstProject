9031e7b0f222ed9a614d92f865545059020cadc0
Dinesh Kumar Chinta <dineshkumarchinta001@gmail.com>
2026-02-03 13:44:54 -0600

    databinding practice

--- src/app/Components/home-page/home-page.component.ts
+++ src/app/Components/home-page/home-page.component.ts
@@ -6,5 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./home-page.component.css']
 })
 export class HomePageComponent {
+title = 'Welcome to the Home Page';
+name:string = '';
 
+city:string = 'New York';
+
+testfn(){
+  return "This is a test function";
+}
+
+invalidfn(){
+  return false;
+}
+
+writeName(event: any){
+  this.name = event.target.value;
+}
+
+alertCity(){
+ this.city = 'Los Angeles';
+}
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HomePageComponent } from "./Components/home-page/home-page.component";
 
 @Component({
   selector: 'app-root',
